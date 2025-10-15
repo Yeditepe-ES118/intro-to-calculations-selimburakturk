35444323f93d6f8f8cfbf6046430f78972e0f2bf
B410-08 <efe.tahtakiran@std.yeditepe.edu.tr>
2025-10-15 15:56:37 +0300

    add lab-3

---
+++ lab3_problem2.py
@@ -0,0 +1,14 @@
+import numpy as np
+def surface_area(pi_app):
+    a = 6378.137 #in km
+    c = 6356.752314245 #in km
+    e_squared = 1-c**2 / a**2
+    s = 2 * pi_app * a**2 * (1+(1-e_squared)/np.sqrt(e_squared) * np.arctanh(np.sqrt(e_squared)))
+    return s
+def compute_error(pi_app1 ,pi_app2):
+    s_1 = surface_area(pi_app1)
+    s_2 = surface_area(pi_app2)
+    error = np.abs(s_1 - s_2)/s_2 * 100 #in %
+    return error
+
+myresult = compute_error(3.14,3.14159)
